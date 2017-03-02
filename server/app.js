39db536310308f378c0d75c2641371df732c6ef1
Taylor Fuqua <tfuqua@gwu.edu>
2017-03-02 10:29:31 -0500

    added assets server rendering

--- server/app.js
+++ server/app.js
@@ -4,24 +4,27 @@ import App from '../src/App';
 import { render, template } from 'rapscallion';
 import { Route, StaticRouter } from "react-router-dom";
 import config from '../config/env/config';
+import assets from '../build/asset-manifest.json';
 
 const port = config.port;
 const app = express();
-
 app.use(express.static('./build'));
 
 
 app.use("*", (req, res) => {
 
+  console.log(assets);
   const context = {};
-
   const markup = render(
     <StaticRouter location={req.url} context={context}>
       <App />
     </StaticRouter>
   );
 
-  const responseRenderer = template`<html><body>${markup}</body></html>`;
+  const responseRenderer = template`<html><head><meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Front-End Boilerplate</title>
+        <link rel="stylesheet" href="/${assets['main.css']}"></head><body>${markup}</body><script src="/${assets['main.js']}"></script></html>`;
   responseRenderer.toStream().pipe(res);
 });
 
